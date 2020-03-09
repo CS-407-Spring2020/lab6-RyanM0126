5b4cded5a48362be4210f542502d617faa5229a3
Ryan Martinez <RyanM0126@github.com>
2020-03-09 13:16:00 -0500

    Milestones1&2

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.android.lab_6;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends FragmentActivity {
+    private final LatLng mDestinationLatLng = new LatLng(43.0715255, -89.4088546);
+    private GoogleMap mMap;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient; //save the instance
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12; //could've been any number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            //code to display marker
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+            displayMyLocation();
+        });
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
     }
+    private void displayMyLocation() {
+        //Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        //If not, ask for it
+        if(permission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        //If permission granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if(task.isSuccessful() && mLastKnownLocation != null){
+                            System.out.println("This is the last known location: " + mLastKnownLocation);
+                            mMap.addPolyline(new PolylineOptions()
+                                    .add(new LatLng(mLastKnownLocation.getLatitude(),
+                                            mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                        }
+                    });
+        }
+    }
+    /**
+     * Handles result of request for location permissions
+     */
+//    @Override
+//    public void onRequestPermissionResult(int requestCode,
+//                                          @NonNull String[] permissions,
+//                                          @NonNull int[] grantResults){
+//        if(requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+//            //if request is cancelled, results array is empty
+//            if(grantResults.length > 0
+//                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                displayMyLocation();
+//            }
+//        }
+//    }
 }
